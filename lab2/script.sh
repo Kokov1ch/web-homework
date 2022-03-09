ab0e6d08fdac67dc01b7ed60609be7ff3a214ad3
Kokov1ch <84625375+Kokov1ch@users.noreply.github.com>
2022-03-10 02:38:52 +1000

    Add files via upload

--- lab2/script.sh
+++ lab2/script.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+name=fedor_fet
+
+echo "My name is $name">>out.txt
+echo "$hero is the best hero in the Dota 2">>out.txt
+echo "BB $1 ">>out.txt
+echo "Hello $2">>out.txt
+find . -name *.txt*
+file out.txt
