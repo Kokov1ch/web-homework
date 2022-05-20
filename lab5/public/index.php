52e2e3574418bc726ab49b6a5266e6465e8613b2
Kokov1ch <84625375+Kokov1ch@users.noreply.github.com>
2022-05-20 19:45:08 +1000

    Create index.php

---
+++ lab5/public/index.php
@@ -0,0 +1,22 @@
+<?php
+spl_autoload_register(function ($className) {
+    $path = dirname(__DIR__ ) . '/src/' . str_replace("\\", "/", $className) . '.php';
+    //echo $path. "<br>";
+    require_once $path;
+});
+use pos3;
+use heroes\mars;
+use heroes\utilities\arena;
+$offlane = new mars();
+$ult = new pos3();
+$arena = new arena();
+echo ("<b>Марс - герой с атрибутом </b>");
+$offlane->print();
+echo "<br>";
+echo ("<b>Ходит на   </b>");
+$ult ->printLine();
+echo ("<b>, роль - </b>");
+$ult ->printRole();
+echo "<br>";
+echo ("<b> Ульта осуществляет </b>");
+$arena->printType();
